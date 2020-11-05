528781d05f78e6daa0c84e11143d9f665460bcb0
aomikan10 <mm1021201@gmail.com>
2020-11-05 20:41:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log('テスト');
+
+const member = {
+  'name': '本田',
+  'height': 170,
+  'hobby': 'サッカー'
+};
+
+console.log(member.hobby);
+
+const member_2 = {
+  '本田':{
+    'height':170,
+    'hobby':'サッカー'
+  },
+  '香川':{
+    'height':165,
+    'hobby':'サッカー'
+  }
+};
+
+console.log(member_2['香川']['height']);
