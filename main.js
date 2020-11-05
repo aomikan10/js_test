dceec17aa9bb3760493029446ba2eece80a6339c
aomikan10 <mm1021201@gmail.com>
2020-11-05 21:11:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const test_1 = 4;
-let test_2 = 3;
+const height = '90';
 
-test_2 += 3;
+console.log(typeof height);
 
-const test_3 = test_1 + test_2;
-
-console.log(test_3);
+if(height === 90){
+  console.log('身長は' + height + 'cmです');
+}
