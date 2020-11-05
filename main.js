e11bc1872a8ec81a37032c97895c0e3fe6dfd7e7
aomikan10 <mm1021201@gmail.com>
2020-11-06 00:57:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const scores = [10, 20, 30];
+const data = 1;
 
-for ( score of scores ){
-  console.log(score);
-}
-
-for (let i = 0; i < 10; i++){
-  console.log(i);
+switch(data){
+  case 1:
+    console.log('1です');
+    break;
+  case 2:
+    console.log('2です');
+    break;
+  case 3:
+    console.log('3です');
+    break;
+  default:
+    console.log('1-3ではありません');
+    break;
 }
