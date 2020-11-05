f510a91d9d7219187a01fd8f5ae45eae68dd7097
aomikan10 <mm1021201@gmail.com>
2020-11-05 22:55:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,9 @@
-const height = 91;
+const scores = [10, 20, 30];
 
-//console.log(typeof height);
-
-// if(height >= 91){
-//   console.log('身長は' + height + 'cmです');
-// }
-
-// if(height < 90){
-//   console.log('身長は' + height + 'cm以下です');
-// }
-
-if(height !== 90){
-  console.log('身長は' + height + 'cmになりません');
+for ( score of scores ){
+  console.log(score);
 }
 
-const signal_1 = 'red';
-const signal_2 = 'yellow';
-
-if(signal_1 === 'red' || signal_2 === 'blue'){
-  console.log('赤');
-}
-
-const score = 80;
-
-const comment = score > 80? 'good' : 'not good';
-
-console.log(comment);
-
-const signal = 'blue';
-
-if(signal === 'red'){
-  console.log('止まれ');
-} else if(signal === 'yellow'){
-  console.log('一旦停止');
-} else {
-  console.log('進む');
-}
-
-const speed = 60;
-
-if(signal === 'blue'){
-  if(speed >= 60){
-    console.log('スピード違反');
-  }
+for (let i = 0; i < 10; i++){
+  console.log(i);
 }
