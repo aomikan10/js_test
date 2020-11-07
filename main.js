6f72b1e14c8587737dde814319666caa9638b2d4
aomikan10 <mm1021201@gmail.com>
2020-11-07 22:41:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-const scores = [10,20,30,40];
+const test = {
+  test1:10,
+  test2:20,
+  test3:30
+};
 
-const newScores = scores.filter((value) => value >= 30);
-
-console.log(newScores);
-
-const members = ['本田','香川','長友'];
-
-const member = members.find((value) => value === '長友');
-
-console.log(member);
-
-const userList = [10,20,30,40];
-
-const userIdList = userList.map((value) => {
-  return `user_${value}`;
-});
+console.log(Object.values(test));
