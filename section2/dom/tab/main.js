9c1217fae6b3e8dd0fd716e96de544e118cda992
aomikan10 <mm1021201@gmail.com>
2020-11-09 10:31:26 +0900

    Update main.js

--- section2/dom/tab/main.js
+++ section2/dom/tab/main.js
@@ -1,10 +1,8 @@
 'use strict';
-
 {
 const tabLabels = document.querySelectorAll('.tab__label li a');
 const tabContents = document.querySelectorAll('.tab__content');
 
-// forEach for-of
 tabLabels.forEach( clickedLabel => {
   clickedLabel.addEventListener('click', e => {
     e.preventDefault();
@@ -12,7 +10,7 @@ tabLabels.forEach( clickedLabel => {
     tabLabels.forEach(label => {
       label.classList.remove('active');
     });
-    
+
     clickedLabel.classList.add('active');
 
     tabContents.forEach(content => {
