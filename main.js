b4fd60d81e9639c5152a95c4b40ac762dc7e9ae9
aomikan10 <mm1021201@gmail.com>
2020-11-09 09:20:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-const globalVariable = 'グローバル';
+'use strict';
 
-function checkScope(){
-  const localVariable = 'ローカル';
+const modal = document.getElementById('modal');
+const show = document.getElementById('modal__show');
+const close = document.getElementById('modal__close');
+const backGround = document.getElementById('modal__background');
 
-  console.log(localVariable);
-}
+show.addEventListener('click', () => {
+  modal.classList.add('is-show');
+  backGround.classList.add('is-show');
+})
 
-console.log(globalVariable);
+close.addEventListener('click', () => {
+  modal.classList.remove('is-show');
+  backGround.classList.remove('is-show');
+})
 
-checkScope();
+backGround.addEventListener('click', () => {
+  close.click();
+})
